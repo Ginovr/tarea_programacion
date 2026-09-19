0ce86da86ce5fe24bbb099602a03074930c1b421
Ginovr <104274692+Ginovr@users.noreply.github.com>
2026-09-19 16:06:27 -0300

    Add files via upload

---
+++ Api_sigeac-Ejercicio2/entity_library/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity_library
+{
+    internal class Person
+    {
+        private string name = "";
+        private int age = 0;
+        private string? dni;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set {  age = value; }
+        }
+
+        public string Dni
+        {
+            get { return dni; }
+            set {  dni = value; }
+        }
+    }
+}
